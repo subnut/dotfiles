ae4c4ba5f28aad79b3bcc40c56985d60eee98e65
Subhaditya Nath <sn03.general@gmail.com>
2021-06-30 12:20:23 +0530

    Update

--- linux/paste.sh
+++ linux/paste.sh
@@ -10,13 +10,15 @@ if test -x /usr/bin/pacman
 then
   PACFLAGS='--needed --noconfirm'
   run sudo pacman -Syu $PACFLAGS '$(cat pacman_installed_packages)'
+  run sudo sed -i '/#Color/s/^#//' /etc/pacman.conf  # Enable color in pacman
   run git clone https://aur.archlinux.org/yay-bin.git --depth 1
   run cd yay-bin '&&' makepkg -si $PACFLAGS
   run rm -rf yay-bin
+  run yay --removemake --save
   run yay -S $PACFLAGS '$(cat AUR_installed_packages)'
   echo
   echo "###### XORG note ######"
-  echo "You don't need dbus or elogind. Uninstall them NOW!"
+  echo "You don't need dbus or elogind. Remove them from Startup Services NOW!"
   echo "Xorg can run perfectly fine without elogind. Use Xorg.wrap"
   echo
   echo "First, ensure that /usr/lib/Xorg.wrap is owned by root"
@@ -25,6 +27,7 @@ then
   echo
   echo "By default /usr/bin/Xorg should be a shell script that simply executes"
   echo "/usr/lib/Xorg.wrap if available, else /usr/lib/Xorg (the actual Xserver)"
+  echo "Do not forget to check Xorg.wrap(1) manpage"
   echo
 fi
 
