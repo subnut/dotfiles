961b9b25a6625d1d1af487acfa3f436df22a22f4
Subhaditya Nath <sn03.general@gmail.com>
2021-07-01 17:33:07 +0530

    Fixed bspwm freezes and desktop_manager

--- common/home/subnut/.config/bspwm/desktop_manager.c
+++ common/home/subnut/.config/bspwm/desktop_manager.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 int
 main(void)
@@ -9,58 +8,55 @@ main(void)
 
 	char cch;	// current char
 	char pch;	// previous char
-	char ppch;	// pre-previous char
 
 	cch  = '\0';
 	pch  = '\0';
-	ppch = '\0';
 
 	count = 0;
 	should_count = 0;
 
-	/* while (!usleep(8000)) */
 	for (;;)
 	{
-		ppch = pch;
-		pch  = cch;
-		cch  = getchar();
-		fputc(cch, stderr);
+		count = 0;
+		should_count = 0;
+
+		for (;;)
+		{
+			pch = cch;
+			cch = getchar();
+
+			if (cch == EOF)
+				break;
+
+			if (cch == '\n')
+				break;
+
+			if (cch == 'M' && pch == ':')
+			{
+				should_count = 1;
+				continue;
+			}
+
+			if (cch == 'm' && pch == ':')
+			{
+				if (should_count == 1)
+					break;
+				else
+					continue;
+			}
+
+			if ((cch == 'F' || cch == 'f') && pch == ':')
+				count++;
+		}
+
+		if (count != 1)
+			if (count > 1)
+				puts("bspc desktop next.!focused.!occupied.local -r");
+			else
+				puts("bspc monitor -a Desktop");
+		fflush(stdout);
 
 		if (cch == EOF)
 			break;
-
-		if (cch == 'M' && (pch == ':' || (pch == 'W' && ppch == '\n')))
-		{
-			should_count = 1;
-			continue;
-		}
-
-		if (cch == 'm' && (pch == ':' || (pch == 'W' && ppch == '\n')))
-		{
-			should_count = 0;
-			continue;
-		}
-
-		if (should_count)
-		{
-			if ((cch == 'f' || cch == 'F') && pch == ':')
-				count++;
-		}
-		else continue;
-
-
-		if (cch == '\n')
-		{
-			if (count > 1)
-				puts("bspc desktop next.!focused.!occupied.local -r");
-			if (count < 1)
-				puts("bspc monitor -a Desktop");
-			fflush(stdout);
-			count = 0;
-			sleep(1);
-			continue;
-		}
 	}
-
-	return 0;
 }
