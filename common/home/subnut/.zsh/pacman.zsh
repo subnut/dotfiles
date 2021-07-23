ecc3451357f09b6c5721dd05ab4ff571468858d2
Subhaditya Nath <sn03.general@gmail.com>
2021-07-23 19:07:04 +0530

    Update

--- common/home/subnut/.zsh/pacman.zsh
+++ common/home/subnut/.zsh/pacman.zsh
@@ -1,9 +1,10 @@
 pacs () { doas pacman -S --needed $(pacman -Ssq $* | fzf --multi --preview-window 'right:50%:hidden:wrap' --preview 'pacman -Si {}') }
-pacr () { doas pacman -Rns $(pacman -Qeq $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Qi {} | grep "Name\|Version\|Description\|Required By\|Optional For\|Install Reason\|Size\|Groups" | cat') }
+pacr () { doas pacman -Rns $(pacman -Qeqt $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Qi {} | grep "Name\|Version\|Description\|Required By\|Optional For\|Install Reason\|Size\|Groups" | cat') }
+pacrr () { doas pacman -Rns $(pacman -Qeq $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Qi {} | grep "Name\|Version\|Description\|Required By\|Optional For\|Install Reason\|Size\|Groups" | cat') }
 # yays () { yay -S $(yay -Ssq $*) --needed }
 # yayss () { yay -Ss $* }
 # alias pacsss="pacman -Ss"
 # pacss() { pacman -Ssq $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Si {}' }
-# pacrr () { doas pacman -R $(pacman -Qq $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Qi {} | grep "Name\|Version\|Description\|Required By\|Optional For\|Install Reason\|Size\|Groups" | cat') }
+# pacrrr () { doas pacman -Rns $(pacman -Qq $* | fzf --multi --preview-window 'right:50%:nohidden:wrap' --preview 'pacman -Qi {} | grep "Name\|Version\|Description\|Required By\|Optional For\|Install Reason\|Size\|Groups" | cat') }
 
 # vim: set nowrap:
